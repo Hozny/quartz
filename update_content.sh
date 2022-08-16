34897188f94f1d3b0a83ad68f0a9e56559679499
Yousof Hosny <yhosnysa@uwaterloo.ca>
2022-08-15 23:22:26 -0400

    Added update script

---
+++ update_content.sh
@@ -0,0 +1,17 @@
+# Copy every 
+if [[ -z "${PATH_TO_EVERYTHING}" ]]; then
+    echo "Failed: PATH_TO_EVERYTHING is not set"
+    exit 1
+fi
+
+# Reset the dir
+rm -rf ./content/*
+mkdir ./content/Zettelkasten
+
+# Assets are not protected from public view
+cp -r "${PATH_TO_EVERYTHING}/templates" "${PATH_TO_EVERYTHING}/Assets" "${PATH_TO_EVERYTHING}/_index.md" ./content
+
+# Copy all non draft files over
+grep -rl "draft:.*false" ${PATH_TO_EVERYTHING}/Zettelkasten | while read -r line ; do 
+    cp "${line}" ./content/Zettelkasten
+done
